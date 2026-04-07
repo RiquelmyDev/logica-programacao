efd4128b8aaa249ff67d63e215fd2a2b79858069
RiquelmyDev <riquelmysvasconcelos@gmail.com>
2026-04-07 11:50:33 -0300

    finalizei meu jogo da velha

--- jogoDaVelha.py
+++ jogoDaVelha.py
@@ -30,7 +30,58 @@ def jogada(line, coluna):
     #    return "O"
     #else:
     #    return "X"
-    
+
+def temGanhador():
+    """Verificação para saber se tem ganhador"""
+
+    for line in range(3):
+        if (
+            tabuleiro[line][0] != " " and
+            tabuleiro[line][0] == tabuleiro[line][1] and
+            tabuleiro[line][0] == tabuleiro[line][2]
+        ):
+            print(f"{tabuleiro[line][0]} GANHOUUUU!!!!!!!! 🎆")
+            return True
+
+    """VERIFICAÇÃO DAS COLUNAS"""
+    for coluna in range(3):
+        if (
+            tabuleiro[0][coluna] != " " and
+            tabuleiro[0][coluna] == tabuleiro[1][coluna] and
+            tabuleiro[0][coluna] == tabuleiro[2][coluna]
+        ):
+            print(f"{tabuleiro[0][coluna]} GANHOUUUU!!!!!!!! 🎆")
+            return True
+        
+        if (
+            tabuleiro[1][1] != " " and
+            (
+                (
+                    tabuleiro[0][0] == tabuleiro[1][1] and
+                    tabuleiro[0][0] == tabuleiro[2][2]
+                ) or
+                (
+                    tabuleiro[0][2] == tabuleiro[1][1] and
+                    tabuleiro[1][1] == tabuleiro[2][0]
+                )
+            )
+        ):
+            
+            print(f"{tabuleiro[1][1]} GANHOUUUU!!!!!!!! 🎆")
+            return True
+
+    """SE NAO TEVE GANHADOR RETORNA FALSO"""
+    return False
+
+def temEmpate():
+    for line in range(3):
+        for coluna in range(3):
+            if tabuleiro[line][coluna] == " ":
+                return False
+    print("Acabou empatado!")
+    return True
+
+
 while True:
     print(f"A vez de jogar é do: ({jogador}) ")
     try:
@@ -41,4 +92,6 @@ while True:
         print("Digite valores numericos entre 0 e 2. ")
     
     showTabuleiro()
-
+    if temGanhador() or temEmpate():
+        break
+    
